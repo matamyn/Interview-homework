f9a30de4a439fdde50a44013073339704a731dfe
t.zaytsev <timofey.zaytsev@stoloto.ru>
2020-08-09 22:03:55 +0300

    test-test

---
+++ CPP/poligon.cpp
@@ -0,0 +1,5 @@
+//
+// Created by timofey on 09.08.2020.
+//
+
+#include "poligon.h"
---
+++ CPP/poligon.h
@@ -0,0 +1,10 @@
+//
+// Created by timofey on 09.08.2020.
+//
+
+#ifndef CPP_HOMEWORK_POLIGON_H
+#define CPP_HOMEWORK_POLIGON_H
+
+class poligon {};
+
+#endif // CPP_HOMEWORK_POLIGON_H
