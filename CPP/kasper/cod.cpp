ad244aab8bb84aa70f8ba86f46bb937f1385d9db
t.zaytsev <timofey.zaytsev@stoloto.ru>
2020-08-11 01:47:06 +0300

    test

--- CPP/kasper/cod.cpp
+++ CPP/kasper/cod.cpp
@@ -1,3 +1,13 @@
+/*
+    Задание
+
+    Указать те ошибки в коде, которые приводят к неправильной работе программы.
+   При этом развернуто и подробно описать, в чем каждая из таких ошибок
+   заключается, как проявляется, к каким именно проблемам приводит. Исправлять
+   найденные ошибки не следует.  Про артефакты, которые не приводят к
+   неправильной работе программы, писать не следует -- за спам снимаем баллы.
+*/
+
 #include <cstdio>
 #include <exception>
 #include <fstream>
@@ -8,11 +18,11 @@
 #include <string>
 #include <thread>
 
-namespace TPROGER {
+namespace TASK6044 {
 typedef std::runtime_error Exception;
 
 //! throws an OsError exception based on errno if bResult is zero
-void Check_errno(bool bResult, const char *file, int line) {
+void Check_errno(bool bResult, const char* file, int line) {
   if (!bResult) {
     fprintf(stderr, "An error %d occurred", errno);
     std::ostringstream os;
@@ -22,11 +32,11 @@ void Check_errno(bool bResult, const char *file, int line) {
 };
 
 //! check last error and throws an OsError exception if res is zero
-#define TPROGER_CHK_ERRNO(res)                                                \
-  TPROGER::Check_errno(!!(res), __FILE__, __LINE__)
+#define TASK6044_CHK_ERRNO(res) \
+  TASK6044::Check_errno(!!(res), __FILE__, __LINE__)
 
 //! throws an OsError exception based on errno if bResult is zero
-void Assertion(bool bResult, const char *szText, const char *file, int line) {
+void Assertion(bool bResult, const char* szText, const char* file, int line) {
   if (!bResult) {
     std::ostringstream os;
     os << "Assertion " << szText << " failed at " << file << "@" << line;
@@ -35,8 +45,8 @@ void Assertion(bool bResult, const char *szText, const char *file, int line) {
 };
 
 //! throws an exception is condition is false
-#define TPROGER_ASSERTION(res)                                                \
-  TPROGER::Assertion(!!(res), #res, __FILE__, __LINE__)
+#define TASK6044_ASSERTION(res) \
+  TASK6044::Assertion(!!(res), #res, __FILE__, __LINE__)
 
 //! Recursive synchronization object
 typedef std::recursive_mutex CritSec;
@@ -54,12 +64,13 @@ public:
 
     \tparam class that must support method 'void TT::Call()'
 */
-template <class T> class AsyncCallImplT {
-  AsyncCallImplT &operator=(const AsyncCallImplT &);
-  AsyncCallImplT(const AsyncCallImplT &);
+template <class T>
+class AsyncCallImplT {
+  AsyncCallImplT& operator=(const AsyncCallImplT&);
+  AsyncCallImplT(const AsyncCallImplT&);
 
 public:
-  AsyncCallImplT(AsyncCallBase *pControl) : m_pControl(pControl) { Create(); };
+  AsyncCallImplT(AsyncCallBase* pControl) : m_pControl(pControl) { Create(); };
 
   ~AsyncCallImplT() { Destroy(); };
 
@@ -70,40 +81,48 @@ private:
 
   void Destroy() {
     if (m_pThread) {
-      TPROGER_ASSERTION(m_pThread->joinable());
+      TASK6044_ASSERTION(m_pThread->joinable());
       m_pThread->join();
-      TPROGER_ASSERTION(!m_pThread->joinable());
+      TASK6044_ASSERTION(!m_pThread->joinable());
       m_pThread = nullptr;
     };
   };
 
   void thread_proc() {
     try {
-      static_cast<T *>(this)->Call();
-      if (this->m_pControl)
-        this->m_pControl->DoCall();
-    } catch (const TPROGER::Exception &excpt) {
-      std::cerr << "TPROGER::Exception occured: " << excpt.what();
-    } catch (const std::exception &excpt) {
+      //приведение типа к AsyncCall_*, проблема восходящего преобразования,
+      //может возникать непроделенное поведение в runtime.
+
+      //static_cast имеет семантику move, в следствии чего будет вызван
+      //деструктор класса TheFactoryImpl, в следствии чего будут вызваны
+      //деструкторы наследуемых классов и композитных объектов, в частности
+      //деструктор AsyncCall_1, который обнулит указатель m_pOutput
+      //у обеъкта типа AsyncCall_0 такой проблемы не будет, т.к. деструктор виртуальный
+      //и  AsyncCallImplT
+      static_cast<T*>(this)->Call();
+      if (this->m_pControl) this->m_pControl->DoCall();
+    } catch (const TASK6044::Exception& excpt) {
+      std::cerr << "TASK6044::Exception occured: " << excpt.what();
+    } catch (const std::exception& excpt) {
       std::cerr << "Unexpected exception: " << excpt.what();
     };
   };
 
 private:
   std::unique_ptr<std::thread> m_pThread;
-  AsyncCallBase *m_pControl;
+  AsyncCallBase* m_pControl;
 };
 
 //! Abstract output class
 class LoggerBase {
 public:
-  virtual void LogLine(const char *szOut) = 0;
+  virtual void LogLine(const char* szOut) = 0;
 };
 
 //! Async action
 class AsyncCall_1 : public AsyncCallImplT<AsyncCall_1> {
 public:
-  AsyncCall_1(LoggerBase *pOutput, AsyncCallBase *pControl)
+  AsyncCall_1(LoggerBase* pOutput, AsyncCallBase* pControl)
       : AsyncCallImplT<AsyncCall_1>(pControl) {
     m_pOutput = pOutput;
   };
@@ -119,13 +138,13 @@ public:
   };
 
 protected:
-  LoggerBase *m_pOutput;
+  LoggerBase* m_pOutput;
 };
 
 //! Async action
 class AsyncCall_0 : public AsyncCallImplT<AsyncCall_0> {
 public:
-  AsyncCall_0(LoggerBase *pOutput, AsyncCallBase *pControl)
+  AsyncCall_0(LoggerBase* pOutput, AsyncCallBase* pControl)
       : AsyncCallImplT<AsyncCall_0>(pControl) {
     m_pOutput = pOutput;
   };
@@ -141,14 +160,14 @@ public:
   };
 
 protected:
-  LoggerBase *m_pOutput;
+  LoggerBase* m_pOutput;
 };
 
 class LoggerImpl : public LoggerBase {
 public:
-  LoggerImpl(CritSec &oRM) : m_pOutput(nullptr), m_oRM(oRM) {
+  LoggerImpl(CritSec& oRM) : m_pOutput(nullptr), m_oRM(oRM) {
     m_pOutput = fopen("result.log", "w");
-    TPROGER_CHK_ERRNO(m_pOutput);
+    TASK6044_CHK_ERRNO(m_pOutput);
   };
 
   ~LoggerImpl() {
@@ -159,16 +178,19 @@ public:
   };
 
   // LoggerBase
-  virtual void LogLine(const char *szOut) {
+  virtual void LogLine(const char* szOut) {
+    //Деструктор класса TheFactoryImpl уже вызвал мьютекс потока,
+    //который в данный момент находится в ожидании завершения.
+    //Из за передачи CritSec по ссылке, произойдет неотслеживаемый deadlock.
     AutoCritSec arm(m_oRM);
     if (m_pOutput) {
-      TPROGER_CHK_ERRNO(EOF != fputs(szOut, m_pOutput));
+      TASK6044_CHK_ERRNO(EOF != fputs(szOut, m_pOutput));
     };
   };
 
 protected:
-  CritSec &m_oRM; // protects class variables
-  FILE *m_pOutput;
+  CritSec& m_oRM;  // protects class variables
+  FILE* m_pOutput;
 };
 
 class TheFactoryImpl : public LoggerImpl, public AsyncCallBase {
@@ -183,6 +205,7 @@ public:
   void Start() {
     AutoCritSec arm(m_oRM);
     m_pAsyncCall1.reset(new AsyncCall_1(this, this));
+    m_pAsyncCall2.reset(new AsyncCall_0(this, this));
   };
 
 protected:
@@ -196,20 +219,20 @@ protected:
   virtual void DoCall() { ; };
 
 protected:
-  CritSec m_oRM; // protects class variables
+  CritSec m_oRM;  // protects class variables
   std::unique_ptr<AsyncCall_1> m_pAsyncCall1;
   std::unique_ptr<AsyncCall_0> m_pAsyncCall2;
-  FILE *m_pOutput;
+  FILE* m_pOutput;
 };
-}
+};  // namespace TASK6044
 
 int main() {
   try {
-    TPROGER::TheFactoryImpl oApp;
-  } catch (const TPROGER::Exception &excpt) {
-    std::cerr << "TPROGER::Exception occured: " << excpt.what();
+    TASK6044::TheFactoryImpl oApp;
+  } catch (const TASK6044::Exception& excpt) {
+    std::cerr << "TASK6044::Exception occured: " << excpt.what();
     return 1;
-  } catch (const std::exception &excpt) {
+  } catch (const std::exception& excpt) {
     std::cerr << "Unexpected exception: " << excpt.what();
     return 3;
   };
--- CPP/poligon.h
+++ CPP/poligon.h
@@ -5,9 +5,9 @@
 using namespace std;
 class A {
 public:
-  A(std::recursive_mutex &mut_) {
+  explicit A(std::recursive_mutex &mut_) {
     cout << "constructor A" << endl;
-    std::lock_guard<std::recursive_mutex> amd1{mut_};
+    std::lock_guard<std::recursive_mutex> amd{mut_};
   }
 };
 class B : public A {
